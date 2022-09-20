33539c3f960686887e21fd52c11e9c494a0b8a78
IljaZarinovs <iz.riga@inbox.lv>
2022-09-20 11:55:15 +0300

    change to Ilja

--- Learning/src/HelloWorld.java
+++ Learning/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world! And hello to all!!!123");
+        System.out.println("My name is Ilja");
     }
 }
